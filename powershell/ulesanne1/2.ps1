cccc1fd8e675293cb4aee64cc41d75d2d9315ac4
pehmo1 <pehmo1@pinecrypt.com>
2022-03-29 23:37:51 +0300

    Finish 2

--- powershell/ulesanne1/2.ps1
+++ powershell/ulesanne1/2.ps1
@@ -1,3 +1,18 @@
-$subFolders = Get-ChildItem -Recurse -Path (Join-Path $PSScriptRoot "PS_Demo") -File
+if(Test-Path (Join-Path $PSScriptRoot "PS_Demo")) {
+  $files = Get-ChildItem -Name -Recurse -Path (Join-Path $PSScriptRoot "PS_Demo") -File
+} else {
+  Write-Host "No files"
+}
 
-Write-Host $subFolders[0]
+foreach($file in $files) {
+  if(([int]($file.ToString().Remove(0,20).Remove(3)) % 2) -ne 0) {
+    # paaritu
+    Move-Item -Path (Join-Path $PSScriptRoot "PS_Demo" $file) -Destination (Join-Path $PSScriptRoot "PS_Demo" "Alamkaust1")
+  } else {
+    # paaris
+    Move-Item -Path (Join-Path $PSScriptRoot "PS_Demo" $file) -Destination (Join-Path $PSScriptRoot "PS_Demo" "Alamkaust2")
+  }
+}
+
+Rename-Item -Path (Join-Path $PSScriptRoot "PS_Demo" "Alamkaust1") -NewName (Join-Path $PSScriptRoot "PS_Demo" "PaaritudFailid")
+Rename-Item -Path (Join-Path $PSScriptRoot "PS_Demo" "Alamkaust2") -NewName (Join-Path $PSScriptRoot "PS_Demo" "PaarisFailid")
